e7cd336039494e91ed626d0ddb91e6990ed64d37
Amar Maharjan <amarm631@gmail.com>
2021-10-10 17:19:03 +0545

    image slider created

---
+++ widgets_library/lib/Screens/components_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ComponentsView extends StatefulWidget {
+  const ComponentsView({Key? key}) : super(key: key);
+
+  @override
+  _ComponentsViewState createState() => _ComponentsViewState();
+}
+
+class _ComponentsViewState extends State<ComponentsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
